3dc3ba61419772e44c5fef8d679545932a1a541a
kasodesyn <bokang.qian@desyn.io>
2023-08-16 17:57:39 +0800

    t

---
+++ contracts/LP.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LP is ERC20 {
+    constructor() ERC20("LP", "LP") {
+        _mint(msg.sender, 2100 ether);
+    }
+}
