240a1d47d0b253835668cd41085e44937709e307
kasodesyn <bokang.qian@desyn.io>
2023-08-16 17:56:38 +0800

    update

--- contracts/IRewardDistributionRecipient.sol
+++ contracts/IRewardDistributionRecipient.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract IRewardDistributionRecipient is Ownable {
     modifier onlyRewardDistribution() {
         require(
-            _msgSender() == address(this),
-            "Caller is not reward distribution"
+            _msgSender() == address(this) || _msgSender() == owner(),
+            "Caller is not reward distribution or owner"
         );
         _;
     }
